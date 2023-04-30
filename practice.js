575c8fde03123879b823d75fe7e1476023cd0cb9
mark garcia <markkusag@outlook.com>
2023-04-30 18:44:25 -0500

    intitial

---
+++ practice.js
@@ -0,0 +1,29 @@
+function multiplyBiggerNumByTwo(num1, num2) {
+const bigNum = num1 > num2 ? num1 : num2;
+  return bigNum * 2;
+}
+
+function divideBiggerNumByThree(num1, num2) {
+  const bigNum = num1 > num2 ? num1 : num2;
+  return bigNum / 3;   
+}
+
+function eatMostTacos(sum1, sum2) {
+  const bigNum = sum1 > sum2 ? sum1 : sum2;
+  return `I ate ${bigNum} tacos.`;
+}
+
+function adoptSmallerDog(weight1, weight2) {
+  const smallDog = weight1 < weight2 ? weight1 : weight2;
+  return `I adopted a dog that weighs ${smallDog} pounds.`;
+}
+
+
+/**************************************************************************/
+/* DO NOT CHANGE THE CODE BELOW */
+module.exports = {
+  multiplyBiggerNumByTwo,
+  divideBiggerNumByThree,
+  eatMostTacos,
+  adoptSmallerDog
+};
